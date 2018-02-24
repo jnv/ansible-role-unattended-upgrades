3e1724c872f3274b9e103ed6cdc342ca0f8b39e8
Jan Vlnas <git@jan.vlnas.cz>
2018-02-24 19:09:49 +0100

    Add Travis folds

--- tests/test.sh
+++ tests/test.sh
@@ -23,6 +23,18 @@ red='\033[0;31m'
 green='\033[0;32m'
 neutral='\033[0m'
 
+section() {
+  echo -e "\033[33;1m$1\033[0m"
+}
+
+fold_start() {
+  echo -e "travis_fold:start:$1\033[33;1m$2\033[0m"
+}
+
+fold_end() {
+  echo -e "\ntravis_fold:end:$1\r"
+}
+
 timestamp=$(date +%s)
 
 tests_dir='tests'
@@ -38,25 +50,29 @@ docker_tag=${distribution}-${version}:ansible
 # Path to playbook inside container
 playbook_path=/etc/ansible/roles/role_under_test/${tests_dir}/${playbook}
 
-printf ${green}"Building Docker container '${docker_tag}'"${neutral}"\n"
+fold_start docker.build "Building Docker container '${docker_tag}'"
 docker build --no-cache --rm --file=${tests_dir}/${dockerfile} --build-arg VERSION=$version --tag=${docker_tag} ./${tests_dir}
-printf ${green}"Starting Docker container with name '${container_id}'"${neutral}"\n"
+fold_end docker.build
+
+fold_start docker.run "Starting Docker container with name '${container_id}'"
 docker run --detach --volume="$PWD":/etc/ansible/roles/role_under_test:rw \
            --name $container_id \
            --privileged -v /sys/fs/cgroup:/sys/fs/cgroup:ro \
            $docker_tag
+fold_end docker.run
 
-printf ${green}"Checking Ansible playbook syntax"${neutral}"\n"
+section "Checking Ansible playbook syntax"
 docker exec --tty $container_id env TERM=xterm ansible-playbook $playbook_path --syntax-check
 
-printf ${green}"Running role"${neutral}"\n"
+section "Running role"
 docker exec $container_id env TERM=xterm env ANSIBLE_FORCE_COLOR=1 ansible-playbook $playbook_path
 
 if [ "$test_idempotence" = true ]; then
   # Run Ansible playbook again (idempotence test).
-  printf ${green}"Idempotence test: Running playbook again"${neutral}"\n"
+  fold_start test.idempotence "Idempotence test: Running playbook again"
   idempotence=$(mktemp)
   docker exec $container_id ansible-playbook $playbook_path | tee -a $idempotence
+  fold_end test.idempotence
   tail $idempotence \
     | grep -q 'changed=0.*failed=0' \
     && (printf ${green}'Idempotence test: pass'${neutral}"\n") \
